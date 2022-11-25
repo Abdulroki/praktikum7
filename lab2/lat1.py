181b117f18c7051aa74e09326283421b2ca182f3
Abdul Rokim <rokiim45@gmail.com>
2022-11-26 03:14:47 +0700

    tambah file semua

---
+++ lab2/lat1.py
@@ -0,0 +1,11 @@
+a = input('Masukan Bilangan Ke-1 : ')
+b = input('Masukan Bilangan Ke-2 : ')
+
+if a > b :
+    print(a, ' bilangan terbesar')
+    
+elif b > a :
+    print(b, ' bilangan terbesar')
+
+else:
+    print("MASUKAN BILANGAN YANG BENAR!!!")
